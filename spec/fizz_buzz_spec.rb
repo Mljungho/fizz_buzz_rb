ef2a5201dd1de1a39bb6ed4d8f918c3686541de2
Mljungho <mathias.ljungholm@gmail.com>
2021-10-20 18:48:36 +0200

    Tweaked the ruby code

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,21 +1,21 @@
 describe FizzBuzz do
 
-    subject { FizzBuzz.new}
+    subject { FizzBuzz.new }
 
     it "is expected to return a number if none of the game rules apply" do 
-        expected{subject.check(1)}.to eq 1
+        expect(subject.check(1)).to eq 1
     end
 
     it "is expected to return 'fizz' if the number is divisable by 3" do
-        expected{subject.check(3)}.to eq "fizz"
+        expect(subject.check(3)).to eq "fizz"
     end
 
     it "is expected to return 'buzz' if the number is divisable by 5" do
-        expected{subject.check(5)}.to eq "buzz"
+        expect(subject.check(5)).to eq "buzz"
     end
 
     it "is expected to return 'fizz-buzz' if the number is divisable by 15" do
-        expected{subject.check(15)}.to eq "fizz-buzz"
+        expect(subject.check(15)).to eq "fizz-buzz"
     end
 
 end
